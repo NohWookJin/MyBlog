5f4c5cbaff3799e16f963068094955865fc49040
NohWookJin <jinn98@kakao.com>
2023-06-08 22:18:12 +0900

    feat: Write Page

--- myblog-client/src/pages/Write.js
+++ myblog-client/src/pages/Write.js
@@ -1,14 +1,22 @@
-import Editor from "../components/write/Editor";
-import TagBox from "../components/write/TagBox";
+import EditorContainer from "../containers/write/EditorContainer";
 import Responsive from "../components/common/Responsive";
+import Preview from "../components/write/Preview";
+import styled from "styled-components";
 
 const Write = () => {
   return (
-    <Responsive>
-      <Editor />
-      <TagBox />
-    </Responsive>
+    <StyledResponsive>
+      <EditorContainer />
+      <Preview />
+    </StyledResponsive>
   );
 };
 
 export default Write;
+
+const StyledResponsive = styled(Responsive)`
+  display: flex;
+  width: 100%;
+`;
+
+// TagBox < Editor < EditorContainer
