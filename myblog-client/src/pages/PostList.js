3c80f64b5413cf02cd3a45d13b6c3e00ad893d05
NohWookJin <jinn98@kakao.com>
2023-06-09 16:00:44 +0900

    feat: PostList 페이지'

--- myblog-client/src/pages/PostList.js
+++ myblog-client/src/pages/PostList.js
@@ -1,8 +1,16 @@
 import React from "react";
 import HeaderContainer from "../containers/common/HeaderContainer";
+import PostListsContainer from "../containers/post/PostListsContainer";
+import PaginationContainer from "../containers/post/PaginationContainer";
 
 const PostList = () => {
-  return <HeaderContainer />;
+  return (
+    <>
+      <HeaderContainer />
+      <PostListsContainer />
+      <PaginationContainer />
+    </>
+  );
 };
 
 export default PostList;
