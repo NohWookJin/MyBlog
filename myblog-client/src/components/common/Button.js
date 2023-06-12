c9ee784b8adb2909764d92fcbec62a1843901c26
NohWookJin <jinn98@kakao.com>
2023-06-12 23:56:00 +0900

    style: button component 수정

--- myblog-client/src/components/common/Button.js
+++ myblog-client/src/components/common/Button.js
@@ -3,7 +3,7 @@ import palette from "../../lib/palette";
 import { Link } from "react-router-dom";
 
 const Button = (props) => {
-  return props.to ? <Link {...props} /> : <StyledButton {...props} />;
+  return props.to ? <StyledLink {...props} /> : <StyledButton {...props} />;
 };
 
 export default Button;
@@ -17,9 +17,33 @@ const StyledButton = styled.button`
   color: white;
   outline: none;
   cursor: pointer;
-
   background: ${palette.teal[6]};
   &:hover {
     background: ${palette.teal[4]};
+  }
+  &:disabled {
+    background: ${palette.gray[3]};
+    color: ${palette.gray[5]};
+    cursor: not-allowed;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  border: none;
+  border-radius: 3px;
+  font-size: 0.75rem;
+  font-weight: bold;
+  padding: 0.25rem 1rem;
+  color: white;
+  outline: none;
+  cursor: pointer;
+  background: ${palette.teal[6]};
+  &:hover {
+    background: ${palette.teal[4]};
+  }
+  &:disabled {
+    background: ${palette.gray[3]};
+    color: ${palette.gray[5]};
+    cursor: not-allowed;
   }
 `;
