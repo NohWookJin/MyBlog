dc4ec4c3136138269020c8a1cf3921ea370675b1
NohWookJin <jinn98@kakao.com>
2023-06-17 21:38:54 +0900

    feat: Pagination

---
+++ myblog-client/src/components/post/Pagination.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import qs from "qs";
+import Button from "../common/Button";
+
+const buildLink = ({ username, tag, page }) => {
+  const query = qs.stringify({ tag, page });
+  return username ? `/@${username}?${query}` : `/?${query}`;
+};
+
+const Pagination = ({ page, lastPage, username, tag }) => {
+  return (
+    <StyledPagination>
+      <Button
+        disabled={page === 1}
+        to={
+          page === 1 ? undefined : buildLink({ username, tag, page: page - 1 })
+        }
+      >
+        이전
+      </Button>
+      <PageNumber>{page}</PageNumber>
+      <Button
+        disabled={page === lastPage}
+        to={
+          page === lastPage
+            ? undefined
+            : buildLink({ username, tag, page: page + 1 })
+        }
+      >
+        다음
+      </Button>
+    </StyledPagination>
+  );
+};
+
+export default Pagination;
+
+const StyledPagination = styled.div`
+  width: 320px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 3rem;
+`;
+
+const PageNumber = styled.div``;
