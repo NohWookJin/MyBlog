3b20db41952dde21cb9b577309fb23fc91b97a90
NohWookJin <jinn98@kakao.com>
2023-06-20 22:33:42 +0900

    feat: Post buttons update

---
+++ myblog-client/src/components/post/PostActionButton.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const StyledPostActionButton = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 2rem;
+  margin-top: -1.5rem;
+`;
+const ActionButton = styled.button`
+  padding: 0.25rem 0.5rem;
+  border-radius: 4px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  font-size: 0.875rem;
+  cursor: pointer;
+  & + & {
+    margin-left: 0.25rem;
+  }
+`;
+
+const PostActionButton = () => {
+  return (
+    <StyledPostActionButton>
+      <ActionButton>수정</ActionButton>
+      <ActionButton>삭제</ActionButton>
+    </StyledPostActionButton>
+  );
+};
+
+export default PostActionButton;
