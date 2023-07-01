c1fecd22a35176866dca3eba4cb8146be4ebeffd
NohWookJin <jinn98@kakao.com>
2023-07-02 02:40:38 +0900

    feat: WritePage Helemt 적용

--- myblog-client/src/pages/Write.js
+++ myblog-client/src/pages/Write.js
@@ -2,10 +2,14 @@ import EditorContainer from "../containers/write/EditorContainer";
 import Responsive from "../components/common/Responsive";
 import Preview from "../components/write/Preview";
 import styled from "styled-components";
+import { Helmet } from "react-helmet-async";
 
 const Write = () => {
   return (
     <StyledResponsive>
+      <Helmet>
+        <title>글 작성하기</title>
+      </Helmet>
       <EditorContainer />
       <Preview />
     </StyledResponsive>
