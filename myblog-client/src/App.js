37fb0d10ebf587880a80b8202e7940d2e6b23bc2
NohWookJin <jinn98@kakao.com>
2023-07-01 20:08:22 +0900

    feat: Helmet Component 추가

--- myblog-client/src/App.js
+++ myblog-client/src/App.js
@@ -4,19 +4,25 @@ import Register from "./pages/Register";
 import Post from "./pages/Post";
 import PostList from "./pages/PostList";
 import Write from "./pages/Write";
+import { Helmet } from "react-helmet-async";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<PostList />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/write" element={<Write />} />
-      <Route path="/:username">
-        <Route index element={<PostList />} />
-        <Route path=":postId" element={<Post />} />
-      </Route>
-    </Routes>
+    <>
+      <Helmet>
+        <title>MyBlog</title>
+      </Helmet>
+      <Routes>
+        <Route path="/" element={<PostList />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/:username">
+          <Route index element={<PostList />} />
+          <Route path=":postId" element={<Post />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
